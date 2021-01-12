57d4985e7786c9d227e609752cbc3cd6acaa9352
nameon <nameonb0507@gmail.com>
2021-01-12 11:31:48 +0900

    Initialize React Project

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>web app service react firebase</h3>
+            </div>
+        );
+    }
+}
+export default App;
