37a4854e2a3f752f5b5052123f8cc4f5aa037270
nameon <nameonb0507@gmail.com>
2021-01-12 16:51:29 +0900

    Add Router Module

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <Card>
+                <CardContent>
+                    React Firebase based on word cloud application
+                </CardContent>
+            </Card>
+        );
+    }
+}
+export default Home;
